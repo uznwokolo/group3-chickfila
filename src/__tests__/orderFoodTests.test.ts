9625e614749d3089df3b151ffcf6c5bfa17591e6
Uzoma Nwokolo <uzo.nwokolo@gmail.com>
2021-08-14 11:35:45 -0500

    completed order food features

--- src/__tests__/orderFoodTests.test.ts
+++ src/__tests__/orderFoodTests.test.ts
@@ -1,6 +1,8 @@
 //import { CFAHomePage } from "./pageObjects/CFAHomePage";
 import { OrderFood } from "./pageObjects/OrderFood";
 import { WebDriver, Capabilities, Builder, By } from "selenium-webdriver";
+import * as testzip from "../data/zipcodes.json";
+import * as meals from "../data/meals.json";
 
 
 
@@ -10,45 +12,62 @@ const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
 const order = new OrderFood(driver);
 
+const grilled = meals[0];
+const nuggets = meals[1];
+
 
 describe("Testing the Order Food feature...", () => {
+    
+    let sampleZip: string = "77037";
+
     beforeEach(async () => {
-        await order.navigate(order.url);
         await order.maxWindow();
+        await order.navigate(order.url);
     });
     afterAll(async () => {
         await order.quit();
     });
-    test.skip("To confirm url is correct", async () => {
+    test("To confirm url is correct", async () => {
         expect(await order.driver.getCurrentUrl()).toBe(order.url);
     });
     // PT4G3-17 - User can select to pickup an order via curbside
     test("To confirm a pickup order is curbside", async () => {
-        await order.pickUpCurbside("77339");
+        await order.pickUpCurbside(sampleZip);
         expect(await order.getText(order.orderTypeInMenu)).toBe("Curbside");
         expect(await order.driver.getCurrentUrl()).toBe(order.menuUrl);
         await order.click(order.changeLocation); // clears the input field
     });
     // PT4G3-18 - User cannot order breakfast after 10:30am
-    test.skip("Cannot order breakfast after 10:30am", async () => {
-        await order.pickUpDriveThru("77339");
+    test("Cannot order breakfast after 10:30am", async () => {
+        await order.pickUpDriveThru(sampleZip);
         await order.click(order.breakfast);
         await order.click(order.spicyBiscuit);
         expect(await order.getText(order.noBreakfast)).toContain("Breakfast items are not available");
         await order.click(order.changeLocation); // clears the input field
     });
-    test.skip("To confirm a pickup order is carry out", async () => {
-        //await order.click(order.changeLocation); // clears the input field
-        await order.pickUpCarryOut("77339");
-        expect(await order.getText(order.orderTypeInMenu)).toBe("Carry-out");
-        expect(await order.driver.getCurrentUrl()).toBe(order.menuUrl);
+    test("To order a meal for carry-out - Grilled Chicken Sandwich", async () => {
+        await order.pickUpCarryOut(sampleZip);
+        await order.selectGrilledMeal();
+        expect(await order.getText(order.mainReview)).toContain(grilled.main);
+        expect(await order.getText(order.sideReview)).toContain(grilled.side);
+        expect(await order.getText(order.bevReview)).toContain(grilled.bev);
+        await order.takeScreenshot("MyFolder_Screenshot/grillreview");
         await order.click(order.changeLocation); // clears the input field
     });
-    test.skip("To confirm a pickup order is dine-in", async () => {
-        await order.pickUpDineIn("77339");
-        expect(await order.getText(order.orderTypeInMenu)).toBe("Dine-in");
-        expect(await order.driver.getCurrentUrl()).toBe(order.menuUrl);
+    test("To order a meal for drive-thru - Nuggets Meal", async () => {
+        await order.pickUpDriveThru(sampleZip);
+        await order.selectNuggetsMeal();
+        expect(await order.getText(order.mainReview)).toContain(nuggets.main);
+        expect(await order.getText(order.sideReview)).toContain(nuggets.side);
+        expect(await order.getText(order.bevReview)).toContain(nuggets.bev);
+        await order.takeScreenshot("MyFolder_Screenshot/nuggsreview");
         await order.click(order.changeLocation); // clears the input field
     });
-    
+    testzip.forEach((loc) => {
+        test("Confirm the name of the first restaurant according to zipcode", async () => {
+            await order.pickUpBasic(loc.zipcode);
+            expect(await order.getText(order.locationInMenu)).toBe(loc.first);
+            await order.click(order.changeLocation); // clears the input field
+        });
+    });
 })
--- src/__tests__/pageObjects/OrderFood.ts
+++ src/__tests__/pageObjects/OrderFood.ts
@@ -2,6 +2,13 @@ import { By, WebDriver } from "selenium-webdriver"
 import { BasePage } from "./BasePage"
 
 
+/*
+export interface Location {
+    zipcode: string;
+    first: number;
+}*/
+
+
 export class OrderFood extends BasePage {
     driver: WebDriver;
     url: string = "https://www.chick-fil-a.com/order";
@@ -16,6 +23,7 @@ export class OrderFood extends BasePage {
     pickupAddr: By = By.name("location");
     findRestaurants: By = By.xpath("//button[@type='submit']");
     selectRestaurant: By = By.xpath("(//span[contains(text(), 'Select this restaurant')])[1]"); // selects first restaurant in the list
+    changeSearch: By = By.xpath("//div[contains(text(), 'Change search')]");
     // Delivery route
     deliveryAddr: By = By.name("addressLine1");
     typeOfBuilding: By = By.name("addressLine2");
@@ -32,9 +40,12 @@ export class OrderFood extends BasePage {
     // Food locators
     meals: By = By.xpath("//button[@data-cy='MOBILE_MEALS']");
     grilledChicken: By = By.xpath("//button[@data-cy='GRILLED_SANDWICH_MEAL']");
+    chickenNuggets: By = By.xpath("//button[@data-cy='NUGGETS_MEAL']");
     chooseSideOrBev: By = By.xpath("//button[@data-cy='NextButton']");
     kaleCrunch: By = By.xpath("//button[@data-cy='KALE_CRUNCH_SIDE']");
+    waffleFries: By = By.xpath("//button[@data-cy='WAFFLE_POTATO_FRIES']");
     lemonade: By = By.xpath("//button[@data-cy='LEMONADE']");
+    sweetTea: By = By.xpath("//button[@data-cy='MEAL_SWEET_TEA']");
     reviewMeal: By = By.xpath("//button[@data-cy='ReviewYourMeal']");
     // Menu items
     breakfast: By = By.xpath("//button[@data-cy='MOBILE_BREAKFAST']");
@@ -42,6 +53,9 @@ export class OrderFood extends BasePage {
     // Error items
     noBreakfast: By = By.xpath("//h5[contains(text(), 'are not available')]");
     // Meal review
+    mainReview: By = By.xpath("(//h5[@data-cy='ReviewMealListItemName'])[1]");
+    sideReview: By = By.xpath("(//h5[@data-cy='ReviewMealListItemName'])[2]");
+    bevReview: By = By.xpath("(//h5[@data-cy='ReviewMealListItemName'])[3]");
     mealPrice: By = By.xpath("//div[@data-cy='MealPrice']");
     addToOrder: By = By.xpath("//button[@data-cy='AddToOrder']");
     cart: By = By.xpath("//button[@data-cy='Cart']");
@@ -75,11 +89,30 @@ export class OrderFood extends BasePage {
         await this.click(this.selectRestaurant);
         await this.click(this.curbside);
     }
-    async pickUpDineIn(zipCodeOrAddr:string){
+    async pickUpBasic(zipCodeOrAddr:string){
         await this.click(this.doPickup);
         await this.sendKeys(this.pickupAddr, zipCodeOrAddr);
         await this.click(this.findRestaurants);
         await this.click(this.selectRestaurant);
         await this.click(this.dineIn);
     }
+    async selectGrilledMeal() {
+        await this.click(this.meals);
+        await this.click(this.grilledChicken);
+        await this.click(this.chooseSideOrBev);
+        await this.click(this.kaleCrunch);
+        await this.click(this.chooseSideOrBev);
+        await this.click(this.sweetTea);
+        await this.click(this.reviewMeal);
+    }
+    async selectNuggetsMeal() {
+        await this.click(this.meals);
+        await this.click(this.chickenNuggets);
+        await this.click(this.chooseSideOrBev);
+        await this.click(this.waffleFries);
+        await this.click(this.chooseSideOrBev);
+        await this.click(this.lemonade);
+        await this.click(this.reviewMeal);
+    }
+
 }
